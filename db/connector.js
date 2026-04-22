0f77032b2864cece30b84b77452d61c95de0cbec
Artem <artem.v.rybchak@ukd.edu.ua>
2026-04-22 18:23:36 +0300

    Initialize database connection and create horoscope table
    Set up PostgreSQL connection and create horoscope table.

---
+++ db/connector.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool({
+    connectionString: process.env.DB_URL,
+});
+
+const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS horoscope (
+    id SERIAL PRIMARY KEY,
+    sign TEXT,
+    month TEXT,
+    text TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`;
+
+pool.query(createTableQuery)
+
+export default pool;
